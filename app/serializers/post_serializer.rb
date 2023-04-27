38cf6cab3047b4d73c8f53f204c75e099c60bb30
Charles Swaleh <charlswall@live.com>
2023-04-27 16:31:49 +0300

    Created the serializers for the post model to show the title, content and tags

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :title, :content
   belongs_to :author
-  has_many :post_tags, serializer: TagSerializer
+  has_many :tags
 
   def short_content
     "#{self.object.content[0..39]}..."
